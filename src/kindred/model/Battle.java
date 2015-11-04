5f254e5d203daee5073abeb1d3456a0823f112c4
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2015-11-04 13:51:23 -0200

    Consertando funções de Batalha

--- src/kindred/model/Battle.java
+++ src/kindred/model/Battle.java
@@ -1,13 +1,13 @@
 package kindred.model;
 
-import java.lang.Math;
 import java.util.Random;
 
 // Calculates damage caused by a Unit to another Unit
 public class Battle {
 
-    private final Random random;      // Random number generator
-    private final double luck = 0.07; // Percentage added or subtracted to damage
+    private final Random random; // Random number generator
+    private final double luck = 0.07; // Percentage added or subtracted to
+                                      // damage
 
     // Initializes random number generator
     public Battle() {
@@ -15,62 +15,65 @@ public class Battle {
     }
 
     /*
-     *  Checks if the attacker will hit the defender and, if so, calculates
-     *  damage caused to defender, reducing its HP.
+     * Checks if the attacker will hit the defender and, if so, calculates
+     * damage caused to defender, reducing its HP.
      */
-    public void execute(Tile attacker, Tile defender) {
+    public int execute(Tile attacker, Tile defender) {
         Unit attackUnit = attacker.getUnit();
         Unit defendUnit = defender.getUnit();
 
         // Calculates agility (with modifiers) and checks if attack will hit
         double attackerAgi = attackUnit.getAgi();
-        attackerAgi *= attacker.getTerrain().getAgilityModifier()/100.0;
+        attackerAgi *= attacker.getTerrain().getAgilityModifier() / 100.0;
         double defenderAgi = defendUnit.getAgi();
-        defenderAgi *= defender.getTerrain().getAgilityModifier()/100.0;
+        defenderAgi *= defender.getTerrain().getAgilityModifier() / 100.0;
 
         if (!checkHit(attackerAgi, defenderAgi))
-            return;
+            return 0;
 
         double atk = attackUnit.getAtk();
         double def = defendUnit.getDef();
-        def *= defender.getTerrain().getDefenseModifier()/100.0;
+        def *= defender.getTerrain().getDefenseModifier() / 100.0;
         double weaponPower = attackUnit.getWeapon().getPower();
 
         // Damage formula
-        double damage = (0.75*atk + 0.5*weaponPower) - def;
-        damage += randomInteger((int) (-luck*damage),
-                                (int) (luck*damage));
+        double damage = (0.75 * atk + 0.5 * weaponPower) - def;
 
-        defendUnit.loseHp(Math.floor(damage));
+        if (damage < 1)
+            return 1;
+
+        damage += randomInteger((int) Math.ceil(-luck * damage),
+                (int) Math.ceil(luck * damage));
+
+        return (int) damage;
     }
 
     /*
-     *  Checks if an attack will hit by comparing the attacker's agility
-     *  with the defender's. Returns 'true' if attack will happen or
-     *  'false' otherwise.
+     * Checks if an attack will hit by comparing the attacker's agility with the
+     * defender's. Returns 'true' if attack will happen or 'false' otherwise.
      */
     private boolean checkHit(double attackerAgi, double defenderAgi) {
-        if (attackerAgi >= 2*defenderAgi) return true;
+        if (attackerAgi >= 2 * defenderAgi)
+            return true;
 
         double hitChance;
 
         // Probability to hit is calculated with sin/cos functions for
         // nonlinearity.
         if (attackerAgi <= defenderAgi) {
-            hitChance = Math.sin((double) (attackerAgi/defenderAgi) * Math.PI/2);
+            hitChance = Math.sin(attackerAgi / defenderAgi * Math.PI / 2);
             hitChance *= 80;
-        }
-        else {
-            hitChance = Math.cos((double) (attackerAgi/defenderAgi) * Math.PI/2);
-            hitChance = (-20*hitChance) + 80;
+        } else {
+            hitChance = Math.cos(attackerAgi / defenderAgi * Math.PI / 2);
+            hitChance = (-20 * hitChance) + 80;
         }
 
         return (randomInteger(1, 100) <= (int) hitChance);
     }
 
     /*
-     *  Randomly generates an integer in the interval [min, max].
-     *  Supposes that min <= max.
+     * Randomly generates an integer in the interval [min, max]. Supposes that
+     * min <= max.
      */
     private int randomInteger(int min, int max) {
         return random.nextInt((max - min) + 1) + min;
--- src/kindred/model/Map.java
+++ src/kindred/model/Map.java
@@ -1,6 +1,5 @@
 package kindred.model;
 
-import java.lang.Math;
 
 /*
  *  Handles events related to a Map in the Game.
@@ -9,9 +8,10 @@ import java.lang.Math;
 public class Map {
 
     private final Tile[][] tiles; // Map is represented as a Tile matrix
-    private final int tileHeight; // Graphical height representation for each tile
-    private final int tileWidth;  // Graphical width representation for each tile
-    private final Battle battle;  // Calculates result of battle between Units
+    private final int tileHeight; // Graphical height representation for each
+                                  // tile
+    private final int tileWidth; // Graphical width representation for each tile
+    private final Battle battle; // Calculates result of battle between Units
 
     // Creates a Map with the specified parameters
     public Map(Tile[][] tiles, int tileHeight, int tileWidth) {
@@ -22,29 +22,32 @@ public class Map {
     }
 
     /*
-     *  Adds a unit to Tile (x, y) on the Map.
-     *  Returns 'true' if placement was successful or 'false' otherwise.
+     * Adds a unit to Tile (x, y) on the Map. Returns 'true' if placement was
+     * successful or 'false' otherwise.
      */
     public boolean placeUnit(Unit unit, int x, int y) {
         // Tile already occupied
-        if (tiles[x][y].getUnit() != null) return false;
+        if (tiles[x][y].getUnit() != null)
+            return false;
 
         tiles[x][y].setUnit(unit);
         return true;
     }
 
     /*
-     *  Moves Unit on Tile (xi, yi) to Tile (xf, yf) on the Map.
-     *  Returns 'true' if movement was successful or 'false' otherwise.
+     * Moves Unit on Tile (xi, yi) to Tile (xf, yf) on the Map. Returns 'true'
+     * if movement was successful or 'false' otherwise.
      */
     public boolean move(int xi, int yi, int xf, int yf) {
         // Tile already occupied
-        if (tiles[xf][yf].getUnit() != null) return false;
+        if (tiles[xf][yf].getUnit() != null)
+            return false;
 
         Unit unit = tiles[xi][yi].getUnit();
-        if (unit == null) return false;
+        if (unit == null)
+            return false;
 
-        // TODO: Check Player the unit belongs to        
+        // TODO: Check Player the unit belongs to
 
         // TODO: If necessary, change name of Unit method
         int move = unit.getMove();
@@ -54,7 +57,8 @@ public class Map {
         int dy = Math.abs(yf - yi);
 
         // Disallow movement to a tile further than Unit's 'move'
-        if (dx + dy > move) return false;
+        if (dx + dy > move)
+            return false;
 
         // Successfully moves unit
         tiles[xi][yi].removeUnit();
@@ -64,13 +68,14 @@ public class Map {
     }
 
     /*
-     *  Makes the Unit on Tile (xi, yi) attack the Unit on Tile (xf, yf).
-     *  Returns 'true' if attack was possible or 'false' otherwise.
+     * Makes the Unit on Tile (xi, yi) attack the Unit on Tile (xf, yf). Returns
+     * damage caused to defending Unit.
      */
-    public boolean attack(int xi, int yi, int xf, int yf) {
+    public int attack(int xi, int yi, int xf, int yf) {
         Unit attacker = tiles[xi][yi].getUnit();
         Unit defender = tiles[xf][yf].getUnit();
-        if (attacker == null || defender == null) return false;
+        if (attacker == null || defender == null)
+            return -1;
 
         // TODO: Check Players the units belongs to
 
@@ -82,12 +87,11 @@ public class Map {
         int dy = Math.abs(yf - yi);
 
         // Disallow attack if distance in greater than range
-        if (dx + dy > range) return false;
+        if (dx + dy > range)
+            return -1;
 
-        // Do battle and check consequences
-        battle.execute(tiles[xi][yi], tiles[xf][yf]);
-        if (defender.getHp() <= 0) tiles[xf][yf].removeUnit();
+        int damage = battle.execute(tiles[xi][yi], tiles[xf][yf]);
 
-        return true;
+        return damage;
     }
 }
