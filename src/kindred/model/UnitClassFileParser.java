5d58da313dcb9c689c78ae5ff8e787b9f566898a
Vinícius Bitencourt Matos <viniciusbm@gamecube.linux.ime.usp.br>
2015-11-13 11:21:29 -0200

    Corrigidos alguns erros nos parsers e adicionado tratamento de erros.

--- src/kindred/model/UnitClassFileParser.java
+++ src/kindred/model/UnitClassFileParser.java
@@ -2,6 +2,7 @@ package kindred.model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -13,50 +14,54 @@ import java.util.regex.Pattern;
 
 public class UnitClassFileParser {
     /**
-     * Not to be imstanciated, since this class is purely static!
+     * Not to be instantiated, since this class is purely static!
      */
-    
+
     private UnitClassFileParser() {
-        //Do not instantiate!
+        // Do not instantiate!
     }
 
-/**
- * Parses a given file containing information about a UnitClass.
- * <p>
- * Creates and returns a Weapon, based on the information contained in the file.
- * 
- * @param filename
- *          Name of the UnitClass file
- * 
- * @retun UnitClass object created based on the information from the file.
- * 
- * @throws FileNotFoundException
- *              If the especified file is not found
- * 
- */
-public static UnitClass parseFile(String filename)
-        throws FileNotFoundException {
-    File f = new File(UnitClass.class.getResource(filename).getPath());
-    Scanner scanner = new Scanner(f);
-    
-    Pattern p = Pattern.compile("\\s*\\w");
-    
-    String name = scanner.next(p);
-    int hp = scanner.nextInt();
-    int att = scanner.nextInt();
-    int def = scanner.nextInt();
-    int agi = scanner.nextInt();
-    int mov = scanner.nextInt();
-    
-    
-    if (scanner.hasNext(p)) {
-            System.err.format("Extra information in '%s'\n", filename);
+    /**
+     * Parses a given file containing information about a UnitClass.
+     * <p>
+     * Creates and returns a Weapon, based on the information contained in the
+     * file.
+     * 
+     * @param filename
+     *            Name of the UnitClass file
+     * 
+     * @return UnitClass object created based on the information from the file
+     * 
+     * @throws FileNotFoundException
+     *             If the specified file is not found
+     * 
+     */
+    public static UnitClass parseFile(String filename) throws FileNotFoundException {
+        File f = new File(UnitClassFileParser.class.getResource(filename).getPath());
+        Scanner scanner = new Scanner(f);
+
+        Pattern p = Pattern.compile("\\s*\\w");
+        String name;
+        int hp, att, def, agi, mov;
+        try {
+            name = scanner.next(p);
+            hp = scanner.nextInt();
+            att = scanner.nextInt();
+            def = scanner.nextInt();
+            agi = scanner.nextInt();
+            mov = scanner.nextInt();
+        } catch (NoSuchElementException e) {
+            System.err.format("Invalid file format in '%s'\n", filename);
+            return null;
+        } finally {
+            scanner.close();
         }
-    
-    Attribute attribute = new Attribute(hp, att, def, agi, mov);
-    UnitClass class = new UnitClass(name, attribute);
-    
-    return class;
-        
+        if (scanner.hasNext(p)) {
+            System.err.format("Warning: Extra information in '%s'\n", filename);
+        }
+        Attribute attribute = new Attribute(hp, att, def, agi, mov);
+        UnitClass unitClass = new UnitClass(name, attribute);
+        scanner.close();
+        return unitClass;
     }
 }
--- src/kindred/model/WeaponFileParser.java
+++ src/kindred/model/WeaponFileParser.java
@@ -2,6 +2,7 @@ package kindred.model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -15,42 +16,49 @@ public class WeaponFileParser {
     /**
      * Not to be imstanciated, since this class is purely static!
      */
-    
+
     private WeaponFileParser() {
-        //Do not instantiate!
+        // Do not instantiate!
     }
 
-/**
- * Parses a given file containing information about a Weapon.
- * <p>
- * Creates and returns a Weapon, based on the information contained in the file.
- * 
- * @param filename
- *          Name of the Weapon file
- * 
- * @retun Weapon object created based on the information from the file.
- * 
- * @throws FileNotFoundException
- *              If the especified file is not found
- * 
- */
-public static Weapon parseFile(String filename)
-        throws FileNotFoundException {
-    File f = new File(Weapon.class.getResource(filename).getPath());
-    Scanner scanner = new Scanner(f);
-    Pattern p = Pattern.compile("\\s*\\w");
-    
-    String name = scanner.next(p);
-    int power = scanner.nextInt();
-    int range = scanner.nextInt();
-    
-    if (scanner.hasNext(p)) {
-            System.err.format("Extra information in '%s'\n", filename);
+    /**
+     * Parses a given file containing information about a Weapon.
+     * <p>
+     * Creates and returns a Weapon, based on the information contained in the
+     * file.
+     * 
+     * @param filename
+     *            Name of the Weapon file
+     * 
+     * @retun Weapon object created based on the information from the file.
+     * 
+     * @throws FileNotFoundException
+     *             If the especified file is not found
+     * 
+     */
+    public static Weapon parseFile(String filename) throws FileNotFoundException {
+        File f = new File(Weapon.class.getResource(filename).getPath());
+        Scanner scanner = new Scanner(f);
+        Pattern p = Pattern.compile("\\s*\\w");
+
+        String name;
+        int power, range;
+        try {
+            name = scanner.next(p);
+            power = scanner.nextInt();
+            range = scanner.nextInt();
+        } catch (NoSuchElementException e) {
+            System.err.format("Invalid file format in '%s'\n", filename);
+            return null;
+        } finally {
+            scanner.close();
         }
-    
-    Weapon weapon = new Weapon(name, power, range);
-    
-    return weapon;
-        
+        if (scanner.hasNext(p)) {
+            System.err.format("Warning: Extra information in '%s'\n", filename);
+        }
+        Weapon weapon = new Weapon(name, power, range);
+        scanner.close();
+        return weapon;
+
     }
 }
